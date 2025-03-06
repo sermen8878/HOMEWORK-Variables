4a07015881ef6fd618dfa0f29204e8ca02930063
sermen8878 <sermen88@yandex.ru>
2025-03-06 22:09:42 +0500

    completed the fourth task

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        dog -= 3.5;
-        cat -= 1.6;
-        paper -= 7639;
-        System.out.println("Новое значение переменной dog: " + dog);
-        System.out.println("Новое значение переменной cat: " + cat);
-        System.out.println("Новое значение переменной paper: " + paper);
+        var friend = 19;  // Переменная friend типа int
+        System.out.println("Начальное значение переменной friend: " + friend);
+        friend += 2;
+        System.out.println("Значение переменной friend после увеличения на 2: " + friend);
+        friend /= 7;
+        System.out.println("Значение переменной friend после деления на 7: " + friend);
     }
-}
+    }

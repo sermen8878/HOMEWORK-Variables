c901708403f4e187879cdb271b95792319fc4554
sermen8878 <sermen88@yandex.ru>
2025-03-06 22:20:09 +0500

    completed the sixth task

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println("Начальное значение переменной frog: " + frog);
-        frog *= 10;
-        System.out.println("Значение переменной frog после увеличения в 10 раз: " + frog);
-        frog /= 3.5;
-        System.out.println("Значение переменной frog после деления на 3.5: " + frog);
-        frog += 4;
-        System.out.println("Значение переменной frog после добавления 4: " + frog);
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        var totalWeight = boxer1Weight + boxer2Weight;
+        var weightDifference = boxer2Weight - boxer1Weight;
+        System.out.println("Общая масса двух бойцов: " + totalWeight + " кг");
+        System.out.println("Разница между массами бойцов: " + weightDifference + " кг");
     }
 }

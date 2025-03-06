7567d915c4e9c9c22a82438929588e30457ae13f
sermen8878 <sermen88@yandex.ru>
2025-03-06 21:41:15 +0500

    completed the second task

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println("Значение переменной dog: " + dog);
-        System.out.println("Значение переменной cat: " + cat);
-        System.out.println("Значение переменной paper: " + paper);
+        dog += 4;
+        cat += 4;
+        paper += 4;
+
+        System.out.println("Новое значение переменной dog: " + dog);
+        System.out.println("Новое значение переменной cat: " + cat);
+        System.out.println("Новое значение переменной paper: " + paper);
     }
 }
